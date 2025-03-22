cd25f8800cd4c02408125d6e40fccf489c9038ae
Noah Olson <21noolson@gmail.com>
2025-03-22 12:35:05 -0500

    Updated the color profile of the website and added the Extension Logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,26 +3,48 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const [show, setShow] = useState(false);
+
+  const handleToggle = () => setShow(prev => !prev);
+  const handleClose = () => setShow(false);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest('.dropdown')) {
+        setShow(false);
+      }
+    };
+
+    if (show) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [show]);
 
   return (
     <nav className="navbar">
       <div className="dropdown">
-        <button className="dropbtn">Menu
-          <i className='fa fa-caret-down'></i>
+        <button onClick={handleToggle} className="dropbtn">
+          Menu <i className="fa fa-caret-down"></i>
         </button>
-        <div className="dropdown-content">
-          <ul className="nav-links">
-            <li><Link to="/" >Home</Link></li>
-            <li><Link to="/FieldHistory" >Field History</Link></li>
-            <li><Link to="/Register" >Registration</Link></li>
-            <li><Link to="/Upload" >Upload Photos</Link></li>
-            <li><Link to="/Map" >View Map</Link></li>
-            <li><Link to="/About" >About</Link></li>
-          </ul>
-        </div>
+        {show && (
+          <div className="dropdown-content">
+            <ul className="nav-links">
+              <li><Link onClick={handleClose} to="/">Home</Link></li>
+              <li><Link onClick={handleClose} to="/FieldHistory">Field History</Link></li>
+              <li><Link onClick={handleClose} to="/Register">Registration</Link></li>
+              <li><Link onClick={handleClose} to="/Upload">Upload Photos</Link></li>
+              <li><Link onClick={handleClose} to="/Map">View Map</Link></li>
+              <li><Link onClick={handleClose} to="/About">About</Link></li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
 }
 
 export default NavBar;
+
