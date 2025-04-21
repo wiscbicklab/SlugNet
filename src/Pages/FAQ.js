af936c9a7312f98bd10cc85a83534899cc9138f3
Noah Olson <21noolson@gmail.com>
2025-04-21 00:31:13 -0500

    Updated the images for creating a Google account with existing email

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -14,30 +14,30 @@ function FAQ() {
     ];
     const imageSrc = {
         "Choose_Google_Account": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Choose_Google_Account_Instructions.png",
-        "Field_History": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Field_History.PNG",
-        "Field_History_Cookies": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Field_History_Cookies.PNG",
+        "Field_History": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Field_History.png",
+        "Field_History_Cookies": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Field_History_Cookies.png",
         "Google_Create_Account": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Account_Instructions.png",
         "Google_Create_Birthday_Gender": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Birthday_Gender_Instructions.png",
-        "Google_Create_Email_Blank": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Email_Blank_Intstructions.png",
-        "Google_Create_Email_Filled": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Email_Filled.PNG",
+        "Google_Create_Email_Blank": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Email_Blank_Instructions.png",
+        "Google_Create_Email_Filled": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Email_Filled.png",
         "Google_Create_Name": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Name_Instructions.png",
-        "Google_Create_Password": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Password_Instructions.PNG",
-        "Google_Create_Recovery": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Recovery_Instructions.PNG",
+        "Google_Create_Password": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Password_Instructions.png",
+        "Google_Create_Recovery": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Recovery_Instructions.png",
         "Google_Existing_Email": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Existing_Email_Instructions.png",
-        "Google_Privacy": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Instructions.PNG",
-        "Google_Privacy_Agree": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Agree_Instructions.PNG",
-        "Google_Privacy_Dont_Save": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Dont_Save_Instructions.PNG",
-        "Google_Privacy_More_Options": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_More_Options_Instructions.PNG",
-        "Google_Register_Finish": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Register_Finished_Instructions.PNG",
-        "Google_Register_Home": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Register_Home_Instructions.PNG",
+        "Google_Privacy": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Instructions.png",
+        "Google_Privacy_Agree": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Agree_Instructions.png",
+        "Google_Privacy_Dont_Save": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_Dont_Save_Instructions.png",
+        "Google_Privacy_More_Options": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Privacy_More_Options_Instructions.png",
+        "Google_Register_Finish": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Register_Finished_Instructions.png",
+        "Google_Register_Home": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Register_Home_Instructions.png",
         "Google_Review_Account": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Create_Account_Instrutions.png",
-        "Google_Sign_In": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Sign_In.PNG",
-        "Google_Suggest_Email": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Suggest_Email.PNG",
-        "Google_Verify_Email": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Verify_Email_Instructions.PNG",
-        "Go_Wisc_Edu": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Go_Wisc.Edu.PNG",
+        "Google_Sign_In": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Sign_In.png",
+        "Google_Suggest_Email": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Suggest_Email.png",
+        "Google_Verify_Email": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Google_Verify_Email_Instructions.png",
+        "Go_Wisc_Edu": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Go_Wisc.Edu.png",
         "Register_Cookies": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Cookies_Instructions.png",
-        "Register_Error": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Error.PNG",
-        "Reigster_Finished": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Finished.PNG",
+        "Register_Error": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Error.png",
+        "Reigster_Finished": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Finished.png",
         "Register_Sign_In": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/Register_Sign_In_Instructions.png",
         "SlugNet_Home": "https://raw.githubusercontent.com/wiscbicklab/SlugNet/refs/heads/main/public/Images/SlugNet_Home_Click_Register.png"
     };
@@ -53,16 +53,16 @@ function FAQ() {
         "Google_Create_Password": "Google will now prompt you to create a password. Create and record or remember a password. Reenter this password in the confirm box below the password box and click next.",
         "Google_Create_Recovery": "Google will now prompt you to add a recovery email. This can be used to recover the account if you forget your password. You may click skip and skip this step.",
         "Google_Existing_Email": "Google will now prompt you to enter your existing email. Enter the email you would like to connect to the account.",
-        "Google_Privacy":"",
-        "Google_Privacy_Agree":"",
-        "Google_Privacy_Dont_Save":"",
-        "Google_Privacy_More_Options":"",
-        "Google_Register_Finish":"",
-        "Google_Register_Home":"",
-        "Google_Review_Account":"",
+        "Google_Privacy": "You will now be on the Google privacy page. Scroll to the bottom.",
+        "Google_Privacy_Agree": "Click the I agree button at the bottom of the privacy page to continue.",
+        "Google_Privacy_Dont_Save": " Under the more options section of the privacy page select Don't Save for Youtube history and Web & Activities. Additionally, select 'Show me ads that arn't personalized. Selecting these options will minimize the amount of information that Google keeps.",
+        "Google_Privacy_More_Options": "Near the bottom of the Google privacy page there should be More Options text. Click this to view more privacy options.",
+        "Google_Register_Finish": "Once you have finished the Registration form, you can click the slugnet redirect link to go back to the website.",
+        "Google_Register_Home": "Now that you have created a Google account you should be on the first page of the project registration form. Click the record email box and then fill out the form.",
+        "Google_Review_Account": "Google will now show you a review of your account. This should show you your email and your name. Confirm that the information is correct and click next.",
         "Google_Sign_In": "Google will now prompt you to Sign In. Instead click on the 'Create account' text next to the 'Next button. This will open a drop-down menu. Please select 'For my personal use from the menu.",
         "Google_Suggest_Email":"",
-        "Google_Verify_Email": "",
+        "Google_Verify_Email": "Google will require you to verify your email. They will send you a verification email with a verification code.",
         "Go_Wisc_Edu":"",
         "Register_Cookies": "Once you navigate to the Google Form you may be prompted to 'Allow cookies'. Please do so if prompted.",
         "Register_Error":"",
@@ -84,18 +84,22 @@ function FAQ() {
                     <img alt={imageAlt["Register_Cookies"]} src={imageSrc["Register_Cookies"]} />
                     <img alt={imageAlt["Register_Sign_In"]} src={imageSrc["Register_Sign_In"]} />
                     <img alt={imageAlt["Choose_Google_Account"]} src={imageSrc["Choose_Google_Account"]} />
+
                     <img alt={imageAlt["Google_Create_Account"]}  src={imageSrc["Google_Create_Account"]} />
                     <img alt={imageAlt["Google_Create_Name"]} src={imageSrc["Google_Create_Name"]} />
                     <img alt={imageAlt["Google_Create_Birthday_Gender"]} src={imageSrc["Google_Create_Birthday_Gender"]} />
                     <img alt={imageAlt["Google_Create_Email_Blank"]} src={imageSrc["Google_Create_Email_Blank"]} />
+
                     <img alt={imageAlt["Google_Existing_Email"]} src={imageSrc["Google_Existing_Email"]} />
                     <img alt={imageAlt["Google_Verify_Email"]} src={imageSrc["Google_Verify_Email"]} />
                     <img alt={imageAlt["Google_Create_Password"]} src={imageSrc["Google_Create_Password"]} />
                     <img alt={imageAlt["Google_Create_Recovery"]} src={imageSrc["Google_Create_Recovery"]} />
+
                     <img alt={imageAlt["Google_Review_Account"]} src={imageSrc["Google_Review_Account"]} />
                     <img alt={imageAlt["Google_Privacy"]} src={imageSrc["Google_Privacy"]} />
                     <img alt={imageAlt["Google_Privacy_More_Options"]} src={imageSrc["Google_Privacy_More_Options"]} />
                     <img alt={imageAlt["Google_Privacy_Dont_Save"]} src={imageSrc["Google_Privacy_Dont_Save"]} />
+
                     <img alt={imageAlt["Google_Privacy_Agree"]} src={imageSrc["Google_Privacy_Agree"]} />
                     <img alt={imageAlt["Google_Register_Home"]} src={imageSrc["Google_Register_Home"]} />
                     <img alt={imageAlt["Google_Register_Finish"]} src={imageSrc["Google_Register_Finish"]} />
@@ -110,20 +114,23 @@ function FAQ() {
                     <img alt={imageAlt["Register_Cookies"]} src={imageSrc["Register_Cookies"]} />
                     <img alt={imageAlt["Register_Sign_In"]} src={imageSrc["Register_Sign_In"]} />
                     <img alt={imageAlt["Choose_Google_Account"]} src={imageSrc["Choose_Google_Account"]} />
+
                     <img alt={imageAlt["Google_Create_Account"]}  src={imageSrc["Google_Create_Account"]} />
                     <img alt={imageAlt["Google_Create_Name"]} src={imageSrc["Google_Create_Name"]} />
                     <img alt={imageAlt["Google_Create_Birthday_Gender"]} src={imageSrc["Google_Create_Birthday_Gender"]} />
                     <img alt={imageAlt["Google_Existing_Email_Filled"]} src={imageSrc["Google_Existing_Email_Filled"]} />
+
                     <img alt={imageAlt["Google_Create_Password"]} src={imageSrc["Google_Create_Password"]} />
                     <img alt={imageAlt["Google_Create_Recovery"]} src={imageSrc["Google_Create_Recovery"]} />
                     <img alt={imageAlt["Google_Review_Account"]} src={imageSrc["Google_Review_Account"]} />
                     <img alt={imageAlt["Google_Privacy"]} src={imageSrc["Google_Privacy"]} />
+
                     <img alt={imageAlt["Google_Privacy_More_Options"]} src={imageSrc["Google_Privacy_More_Options"]} />
                     <img alt={imageAlt["Google_Privacy_Dont_Save"]} src={imageSrc["Google_Privacy_Dont_Save"]} />
                     <img alt={imageAlt["Google_Privacy_Agree"]} src={imageSrc["Google_Privacy_Agree"]} />
                     <img alt={imageAlt["Google_Register_Home"]} src={imageSrc["Google_Register_Home"]} />
-                    <img alt={imageAlt["Google_Register_Finish"]} src={imageSrc["Google_Register_Finish"]} />
 
+                    <img alt={imageAlt["Google_Register_Finish"]} src={imageSrc["Google_Register_Finish"]} />
                 </div>
             }
         </div>
